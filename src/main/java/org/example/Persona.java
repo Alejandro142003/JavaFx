3cb9ab26fa184f3bd754ef1262e44df61a4cf63d
Alejandro <gutierrezcrucesalejandro@gmail.com>
2023-04-13 19:51:11 +0200

    Añado las vistas de administrador

---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,51 @@
+package com.example.demo200;
+import javafx.fxml.FXML;
+
+import java.util.Objects;
+public class Persona {
+    private String nombre,apellidos,edad;
+    public Persona(String nombre, String apellidos,String edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+
+        this.apellidos = apellidos;
+    }
+    public String getEdad() {
+        return edad;
+    }
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(apellidos, edad, nombre);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Persona other = (Persona) obj;
+        return Objects.equals(apellidos, other.apellidos) && edad == other.edad && Objects.equals(nombre, other.nombre);
+    }
+
+}
---
+++ src/main/java/org/example/PrimaryController.java
@@ -0,0 +1,74 @@
+package com.example.demo200;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class PrimaryController  implements Initializable{
+
+	   @FXML
+	    private Button btnAgregar;
+
+	    @FXML
+	    private TableColumn<Persona,String> colApellidos;
+
+	    @FXML
+	    private TableColumn<Persona,String> colEdad;
+
+	    @FXML
+	    private TableColumn <Persona,String>colNombre;
+
+	    @FXML
+	    private TableView<Persona> tbPersonas;
+
+	    @FXML
+	    private TextField txtApellidos;
+
+	    @FXML
+	    private TextField txtEdad;
+
+	    @FXML
+	    private TextField txtNombre;
+	    
+	    private ObservableList<Persona> personas=FXCollections.observableArrayList();
+
+/*arraylist de fx*/
+@Override
+	public void initialize(URL url,ResourceBundle rb) {
+
+	    	colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+
+			colApellidos.setCellValueFactory(new PropertyValueFactory<>("apellidos"));
+
+			colEdad.setCellValueFactory(new PropertyValueFactory<>("edad"));
+
+	    }
+
+	    @FXML
+	    private void agregarPersona(ActionEvent event) {
+			if(!"".equals(txtNombre.getText().trim())){
+				personas.add(new Persona(txtNombre.getText().trim(),txtApellidos.getText().trim(),txtEdad.getText().trim()));
+				tbPersonas.setItems(personas);
+				tbPersonas.refresh();
+				txtNombre.setText("");
+				txtApellidos.setText("");
+				txtEdad.setText("");
+			}else{
+				Alert alerta=new Alert(Alert.AlertType.INFORMATION);
+				alerta.setHeaderText("Mensaje de informacion");
+				alerta.setTitle("Dialogo de advertencia");
+				alerta.setContentText("Es necesario que se escriba un nombre al contacto");
+				alerta.showAndWait();
+			}
+		}
+}
